bb1039deeb4c3e1e4467cf60b3f314f1508afbe1
Kayla Dailey <kayladailey000@gmail.com>
2019-11-12 23:31:51 -0600

    Set up project and  cleaned up app.js file

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>The Dailey Craze</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
