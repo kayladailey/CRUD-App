730fba7e43c1f2e98e54d66857debe1976c76dca
Kayla Dailey <kayladailey000@gmail.com>
2020-01-16 00:17:36 -0600

    Restart as tutorial

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React from 'react';
+import UserTable from './tables/UserTables';
+import {useState} from 'react';
 
-const App = () => {
+
+
+const App = (props) => {
+
+  //mock data below is being used as out initialstate in our useState hook
+  const usersData = [
+    { id: 1, name: 'Kayla', username: 'daileyfro' },
+    { id: 2, name: 'Nick', username: 'nick_duh' },
+    { id: 3, name: 'Lincoln', username: 'blackpanther'},
+    { id: 4, name: 'Mackenzie', username: 'princessjune'},
+  ]
+
+  const [users, setUsers] = useState(usersData)
+
   return (
     <div className="container">
       <h1>The Dailey Craze</h1>
@@ -10,6 +25,7 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
+          <UserTable users=/>
         </div>
       </div>
     </div>
